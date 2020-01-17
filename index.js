98ee1b5ccb0f88f0674e3be5eed8bdbb35d5038f
hugo <hugojerez21@gmail.com>
2020-01-17 01:12:05 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,46 @@
+// Lodash + Flatten + Unflatten + JSON safe coding
+
+const lodash = require('lodash')
+var FP = require('lodash/fp');
+
+lodash.mixin({
+    flat(input) {
+        const result = {}
+        const checkValue = (obj, path = '') => {
+            lodash.forEach(obj, (value, index) => {
+                const currentPath = path + index + '.'
+                // Avoid collisions with universal packages like Moment.js
+                if (lodash.isObject(value) && !lodash.get(value, '_isAMomentObject')) {
+                    checkValue(value, currentPath)
+                } else {
+                    result[lodash.join(lodash.slice(currentPath, 0, -1), '')] = value
+                }
+            })
+        }
+        checkValue(input)
+        return result
+    },
+    unflat(input) {
+        return FP.flow([
+            FP.toPairs,
+            FP.reduce((cum, [key, value]) => _.set(cum, key, value), {}),
+        ]);
+    },
+    encode(obj) {
+        const result = lodash.attempt(JSON.stringify.bind(null, obj))
+        if (lodash.isError(result)) {
+            return undefined
+        } else {
+            return result
+        }
+    },
+    decode(obj) {
+        const result = lodash.attempt(JSON.parse.bind(null, obj))
+        if (lodash.isError(result)) {
+            return undefined
+        } else {
+            return result
+        }
+    }
+})
+module.exports = lodash
